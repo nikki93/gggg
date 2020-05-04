4de87efd75f862b00994da466538a79ed73cda6e
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-05-03 20:32:28 -0700

    rects bench

--- App.js
+++ App.js
@@ -44,31 +44,74 @@ const Canvas = () => {
       const ctx = canvasRef.current.getContext('2d');
 
       // High-DPI
-      canvas.width = 2 * canvas.width;
-      canvas.height = 2 * canvas.height;
-      ctx.scale(2, 2);
+      const W = canvas.width,
+        H = canvas.height;
+      canvas.width = devicePixelRatio * W;
+      canvas.height = devicePixelRatio * H;
+      ctx.scale(devicePixelRatio, devicePixelRatio);
 
       // Init
+      const N = 30000;
+      const rects = [];
+      {
+        // Rects
+        for (let i = 0; i < N; ++i) {
+          rects[i] = {
+            x: W * Math.random(),
+            y: H * Math.random(),
+            fillStyle: `rgb(${Math.floor(255 * Math.random())}, ${Math.floor(
+              255 * Math.random()
+            )}, ${Math.floor(255 * Math.random())})`,
+            speed: 0.5 * W * Math.random(),
+            w: (W * Math.random()) / 16,
+            h: (W * Math.random()) / 16,
+            phase: 2 * Math.PI * Math.random(),
+          };
+        }
+      }
 
       // Update
       let lastUpdateTime = 0.001 * performance.now();
+      let lastFPSUpdateTime = lastUpdateTime;
+      let nFramesSinceLastFPSUpdate = 0;
+      let fps = 0;
       const update = () => {
+        // Time
         const t = 0.001 * performance.now();
         const dt = t - lastUpdateTime;
         lastUpdateTime = t;
+        ++nFramesSinceLastFPSUpdate;
+        if (t - lastFPSUpdateTime > 1) {
+          fps = nFramesSinceLastFPSUpdate / (t - lastFPSUpdateTime);
+          lastFPSUpdateTime = t;
+          nFramesSinceLastFPSUpdate = 0;
+        }
+
+        // Rects
+        for (let i = 0; i < N; ++i) {
+          const rect = rects[i];
+          rect.x += rect.speed * Math.sin(t + rect.phase) * dt;
+        }
       };
 
       // Draw
       const draw = () => {
+        // Clear
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = 'white';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        ctx.fillStyle = 'rgb(200, 0, 0)';
-        ctx.fillRect(10, 10, 50, 50);
+        // Rects
+        for (let i = 0; i < N; ++i) {
+          const rect = rects[i];
+          ctx.fillStyle = rect.fillStyle;
+          ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
+        }
 
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        ctx.fillRect(30, 30, 50, 50);
+        // FPS
+        ctx.fillStyle = 'black';
+        ctx.font = '12px sans';
+        ctx.fillText(`fps: ${fps.toFixed(2)}`, 20, 20);
       };
 
       // Frame loop
