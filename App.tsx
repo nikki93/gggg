778eb985c4e8fdf033625532726898160e281289
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-05-04 14:24:24 -0700

    remove own service worker code

--- App.tsx
+++ App.tsx
@@ -280,12 +280,4 @@ const App = () => {
   return;
 };
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for (let registration of registrations) {
-      registration.update();
-    }
-  });
-}
-
 export default App;
