37d1df208d4eeed9e1a1a6decd8d7c1f2a954d31
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-05-03 23:04:03 -0700

    code cleanup

--- App.js
+++ App.js
@@ -43,102 +43,97 @@ const UI_BACKGROUND_COLOR = 'white';
 const CANVAS_BACKGROUND_COLOR = '#fdf9f5';
 
 const Canvas = () => {
-  const canvasRef = useRef(null);
+  const setCanvasRef = useCallback((canvas) => {
+    if (!canvas) {
+      return;
+    }
+    const ctx = canvas.getContext('2d');
 
-  const setCanvasRef = useCallback((newRef) => {
-    canvasRef.current = newRef;
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvasRef.current.getContext('2d');
+    const setup = () => {
+      // Wait until non-zero size
+      let W = canvas.width;
+      let H = canvas.height;
+      if (W == 0 || H == 0) {
+        requestAnimationFrame(setup);
+        return;
+      }
 
-      const setup = () => {
-        // Wait until non-zero size
-        let W = canvas.width;
-        let H = canvas.height;
-        if (W == 0 || H == 0) {
-          requestAnimationFrame(setup);
-          return;
+      // Init
+      const N = 5;
+      const rects = [];
+      {
+        // Rects
+        for (let i = 0; i < N; ++i) {
+          rects[i] = {
+            x: W * Math.random(),
+            y: H * Math.random(),
+            fillStyle: `rgb(${Math.floor(255 * Math.random())}, ${Math.floor(
+              255 * Math.random()
+            )}, ${Math.floor(255 * Math.random())})`,
+            speed: 0.5 * W * Math.random(),
+            w: (W * Math.random()) / 16,
+            h: (W * Math.random()) / 16,
+            phase: 2 * Math.PI * Math.random(),
+          };
+        }
+      }
+
+      // Update
+      let lastUpdateTime = 0.001 * performance.now();
+      let lastFPSUpdateTime = lastUpdateTime;
+      let nFramesSinceLastFPSUpdate = 0;
+      let fps = 0;
+      const update = () => {
+        // Time
+        const t = 0.001 * performance.now();
+        const dt = t - lastUpdateTime;
+        lastUpdateTime = t;
+        ++nFramesSinceLastFPSUpdate;
+        if (t - lastFPSUpdateTime > 1) {
+          fps = nFramesSinceLastFPSUpdate / (t - lastFPSUpdateTime);
+          lastFPSUpdateTime = t;
+          nFramesSinceLastFPSUpdate = 0;
         }
 
-        // Init
-        const N = 5;
-        const rects = [];
-        {
-          // Rects
-          for (let i = 0; i < N; ++i) {
-            rects[i] = {
-              x: W * Math.random(),
-              y: H * Math.random(),
-              fillStyle: `rgb(${Math.floor(255 * Math.random())}, ${Math.floor(
-                255 * Math.random()
-              )}, ${Math.floor(255 * Math.random())})`,
-              speed: 0.5 * W * Math.random(),
-              w: (W * Math.random()) / 16,
-              h: (W * Math.random()) / 16,
-              phase: 2 * Math.PI * Math.random(),
-            };
-          }
+        // Rects
+        for (let i = 0; i < N; ++i) {
+          const rect = rects[i];
+          rect.x += rect.speed * Math.sin(t + rect.phase) * dt;
+        }
+      };
+
+      // Draw
+      const draw = () => {
+        // Clear
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = CANVAS_BACKGROUND_COLOR;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Rects
+        for (let i = 0; i < N; ++i) {
+          const rect = rects[i];
+          ctx.fillStyle = rect.fillStyle;
+          ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
         }
 
-        // Update
-        let lastUpdateTime = 0.001 * performance.now();
-        let lastFPSUpdateTime = lastUpdateTime;
-        let nFramesSinceLastFPSUpdate = 0;
-        let fps = 0;
-        const update = () => {
-          // Time
-          const t = 0.001 * performance.now();
-          const dt = t - lastUpdateTime;
-          lastUpdateTime = t;
-          ++nFramesSinceLastFPSUpdate;
-          if (t - lastFPSUpdateTime > 1) {
-            fps = nFramesSinceLastFPSUpdate / (t - lastFPSUpdateTime);
-            lastFPSUpdateTime = t;
-            nFramesSinceLastFPSUpdate = 0;
-          }
+        // FPS
+        ctx.fillStyle = 'black';
+        ctx.font = '24px sans';
+        ctx.fillText(`fps: ${fps.toFixed(2)}`, 24, 48);
+      };
 
-          // Rects
-          for (let i = 0; i < N; ++i) {
-            const rect = rects[i];
-            rect.x += rect.speed * Math.sin(t + rect.phase) * dt;
-          }
-        };
-
-        // Draw
-        const draw = () => {
-          // Clear
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.fillStyle = CANVAS_BACKGROUND_COLOR;
-          ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-          // Rects
-          for (let i = 0; i < N; ++i) {
-            const rect = rects[i];
-            ctx.fillStyle = rect.fillStyle;
-            ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
-          }
-
-          // FPS
-          ctx.fillStyle = 'black';
-          ctx.font = '24px sans';
-          ctx.fillText(`fps: ${fps.toFixed(2)}`, 24, 48);
-        };
-
-        // Frame loop
-        const frame = () => {
-          W = canvas.width;
-          H = canvas.height;
-          update();
-          draw();
-          requestAnimationFrame(frame);
-        };
+      // Frame loop
+      const frame = () => {
+        W = canvas.width;
+        H = canvas.height;
+        update();
+        draw();
         requestAnimationFrame(frame);
       };
-      setup();
-    }
+      requestAnimationFrame(frame);
+    };
+    setup();
   }, []);
-
-  useEffect(() => {}, [canvasRef]);
 
   // Canvas centered in a box
   return (
